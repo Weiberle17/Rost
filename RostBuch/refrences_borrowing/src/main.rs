98329f297a6387207edbfa87cd4863b233fd3587
Weiberle17 <maxweiberle63@gmail.com>
2023-06-11 20:06:01 +0200

    feat: Added a few functions

--- RostBuch/refrences_borrowing/src/main.rs
+++ RostBuch/refrences_borrowing/src/main.rs
@@ -1,6 +1,9 @@
 fn main() {
   let mut v = vec!['h', 'a', 'l', 'l', 'o'];
   ascii_capitalize(&mut v);
+  recap_1();
+  recap_slice();
+  recap_moving_strings();
 }
 
 fn ascii_capitalize(v: &mut Vec<char>) {
@@ -11,4 +14,35 @@ fn ascii_capitalize(v: &mut Vec<char>) {
   } else {
     println!("Already capitalized: {:?}", v);
   }
+}
+
+fn recap_1() {
+  let mut a_num = 0;
+  recap_inner(&mut a_num);
+  println!("{a_num}");
+}
+
+fn recap_inner(x: &mut i32) {
+  let another_num = 1;
+  let a_stack_ref = &another_num;
+
+  let a_box = Box::new(2);
+  let a_box_stack_ref = &a_box;
+  let a_box_heap_ref = &*a_box;
+
+  *x += 5;
+}
+
+fn recap_slice() {
+  let s = String::from("abcdefg");
+  let s_slice = &s[2..5];
+  println!("{s}, {s_slice}");
+}
+
+fn recap_moving_strings() {
+  let s = String::from("Hello World");
+  let cs = &s;
+
+  println!("{cs}");
+  println!("{s}");
 }
