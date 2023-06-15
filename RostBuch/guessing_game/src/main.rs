b75e62fd1fd1ff7664e86c0451d0bf4bd18300b1
Weiberle17 <maxweiberle63@gmail.com>
2023-06-15 14:35:51 +0200

    feat: Added proper error handling

--- RostBuch/guessing_game/src/main.rs
+++ RostBuch/guessing_game/src/main.rs
@@ -2,6 +2,22 @@ use rand::Rng;
 use std::{cmp::Ordering, io};
 
 fn main() {
+  pub struct Guess {
+    value: i32,
+  }
+
+  impl Guess {
+    pub fn new(value: i32) -> Result<Guess, String> {
+      if value < 1 || value > 100 {
+        Err(format!("Guess must be between 1 and 100, got {}", value))?;
+      }
+      Ok(Guess { value })
+    }
+
+    pub fn value(&self) -> i32 {
+      self.value
+    }
+  }
   println!("This is a guessing game");
 
   let number_to_guess = rand::thread_rng().gen_range(1..=100);
@@ -15,14 +31,22 @@ fn main() {
       .read_line(&mut guessed_number)
       .expect("User input failed!");
 
-    let guessed_number: u32 = match guessed_number.trim().parse() {
-      Ok(num) => num,
+    let guessed_number: Guess = match guessed_number.trim().parse() {
+      Ok(num) => {
+        match Guess::new(num) {
+          Ok(guess) => guess,
+          Err(e) => {
+            println!("{e}");
+            continue;
+          }
+        }
+      },
       Err(_) => continue,
     };
 
-    println!("You guessed: {guessed_number}");
+    println!("You guessed: {}", guessed_number.value());
 
-    match guessed_number.cmp(&number_to_guess) {
+    match guessed_number.value().cmp(&number_to_guess) {
       Ordering::Less => println!("Too small!"),
       Ordering::Greater => println!("Too big!"),
       Ordering::Equal => {
