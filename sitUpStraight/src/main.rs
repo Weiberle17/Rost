fcd584f26ff2c71ba6152cf51f1e71c6584b8ea1
Weiberle17 <maxweiberle63@gmail.com>
2023-06-19 12:30:13 +0200

    feat: Script to remind me

--- sitUpStraight/src/main.rs
+++ sitUpStraight/src/main.rs
@@ -1,11 +1,18 @@
-use notify_rust::{Notification, Timeout};
+use std::{thread::sleep, time::Duration};
+
+use notify_rust::Notification;
 
 fn main() {
-  Notification::new()
-    .summary("Sit up Straight!")
-    .body("You are supposed to sit up straight!")
-    .icon("sitting.png")
-    .timeout(Timeout::Never)
-    .show()
-    .expect("Failed to show Notification");
+  const TIMER: Duration = Duration::from_secs(600);
+  loop {
+    Notification::new()
+      .summary("Sit up Straight!")
+      .hint(notify_rust::Hint::Transient(true))
+      .body("You are supposed to sit up straight!")
+      .show()
+      .expect("Failed to show Notification")
+      .on_close(|| println!("Notification closed"));
+    
+    sleep(TIMER);
+  }
 }
